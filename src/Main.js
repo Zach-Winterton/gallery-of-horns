dd3399ae99150c733911227cd0905a07c97adb4e
Zach-Winterton <schenley67@gmail.com>
2021-07-21 22:23:13 -0600

    added drop down search menu

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Form from 'react-bootstrap/Form';
 
 class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchBarHorns: this.props.data
+    }
+  }
+  hornFilter = e => {
+    let list = this.props.data;
+      if(e.target.value){
+        list = list.filter(hornedAnimal => hornedAnimal.horns === parseInt(e.target.value));
+      }
+      this.setState({
+        searchBarHorns: list,
+      })
+    
+  }
 
   render() {
     let gallery = [];
-    this.props.data.forEach((itemInformation, index) => {
+    this.state.searchBarHorns.forEach((itemInformation, index) => {
       let hornedAnimal = <HornedBeast
       key={index}
       ShowModal={this.props.ShowModal}
@@ -20,6 +37,18 @@ class Main extends React.Component {
 
     return (
       <main>
+      <Form>
+        <Form.Group>
+              <Form.Label>Please choose the number of horns that you would like to be displayed:</Form.Label>
+              <Form.Control as ="Select" custom onChange = {this.hornFilter}>
+              <option value="">Show all horned creatures</option>
+              <option value="1">One horned creatures</option>
+              <option value="2">Two horned creatures</option>
+              <option value="3">Three horned creatures</option>
+              <option value="100">One hundred horned creatures</option>
+              </Form.Control>
+            </Form.Group>
+           </Form>
         <CardColumns>
         {gallery}
         </CardColumns>
